318748dab52f9fcd96bc4295b90f6b481b29a103
nbari <nbari@tequila.io>
2016-12-10 19:43:58 +0100

    modified:   generic_zfs.sh

--- zfs/generic_zfs.sh
+++ zfs/generic_zfs.sh
@@ -18,7 +18,7 @@ gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i 1 ${mddev}
 
 sysctl vfs.zfs.min_auto_ashift=12
 
-zpool create -o cachefile=/var/tmp/zpool.cache -o altroot=/mnt -o autoexpand=on -O compress=lz4 -O atime=off -O zroot /dev/gpt/disk0
+zpool create -o cachefile=/var/tmp/zpool.cache -o altroot=/mnt -o autoexpand=on -O compress=lz4 -O atime=off zroot /dev/gpt/disk0
 zfs create -o mountpoint=none zroot/ROOT
 zfs create -o mountpoint=/ zroot/ROOT/default
 zfs create -o mountpoint=/tmp -o exec=on -o setuid=off zroot/tmp

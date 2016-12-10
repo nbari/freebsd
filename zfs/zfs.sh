1a6a578cda65a91443746116a922ef36df49a49f
nbari <nbari@tequila.io>
2016-12-10 16:46:20 +0100

    modified:   zfs.sh

--- zfs/zfs.sh
+++ zfs/zfs.sh
@@ -11,8 +11,8 @@ truncate -s ${VMSIZE} ${RAW}
 mddev=$(mdconfig -a -t vnode -f ${RAW})
 
 gpart create -s gpt ${mddev}
-gpart add -a 4k -s 1024 -t freebsd-boot ${mddev}
-gpart add -a 4k -t freebsd-zfs -l disk0 ${mddev}
+gpart add -a 4k -s 512k -t freebsd-boot ${mddev}
+gpart add -a 1m -t freebsd-zfs -l disk0 ${mddev}
 gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i 1 ${mddev}
 
 sysctl vfs.zfs.min_aut_ashift=12

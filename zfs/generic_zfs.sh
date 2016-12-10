f6a66c35cc7a22a3da09e64a8984d440c7ba59ed
nbari <nbari@tequila.io>
2016-12-10 19:37:58 +0100

    modified:   generic_zfs.sh

--- zfs/generic_zfs.sh
+++ zfs/generic_zfs.sh
@@ -18,7 +18,7 @@ gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i 1 ${mddev}
 
 sysctl vfs.zfs.min_auto_ashift=12
 
-zpool create -o cachefile=/var/tmp/zpool.cache -o altroot=/mnt -o autoexpand=on -O compress=lz4 -O atime=off -O utf8only=on zroot /dev/gpt/disk0
+zpool create -o cachefile=/var/tmp/zpool.cache -o altroot=/mnt -o autoexpand=on -O compress=lz4 -O atime=off -O zroot /dev/gpt/disk0
 zfs create -o mountpoint=none zroot/ROOT
 zfs create -o mountpoint=/ zroot/ROOT/default
 zfs create -o mountpoint=/tmp -o exec=on -o setuid=off zroot/tmp
@@ -30,6 +30,7 @@ zfs create -o mountpoint=/var -o canmount=off zroot/var
 zfs create -o exec=off -o setuid=off zroot/var/audit
 zfs create -o exec=off -o setuid=off zroot/var/crash
 zfs create -o exec=off -o setuid=off zroot/var/log
+zfs create -o exec=off -o setuid=off -o readonly=on zroot/var/empty
 zfs create -o atime=on zroot/var/mail
 zfs create -o setuid=off zroot/var/tmp
 zfs create zroot/var/ports
@@ -130,6 +131,7 @@ console="comconsole"
 hw.broken_txfifo="1"
 zfs_load="YES"
 vfs.root.mountfrom="zfs:zroot/ROOT/default"
+vfs.root.mountfrom.options="rw"
 EOF
 
 # /etc/rc.conf

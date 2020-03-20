19f5f4145626d41340c85f11a8aa52d09fb5721b
nbari <nbari@tequila.io>
2020-03-20 19:36:10 +0100

    sync Fri Mar 20 19:36:10 CET 2020

--- kernels/update.sh
+++ kernels/update.sh
@@ -2,6 +2,7 @@
 FREEBSD_VERSION=12
 NUMBER_OF_CORES=`sysctl -n hw.ncpu`
 ZPOOL="tank"
+START=$(date +%s)
 
 svnlite co svn://svn.freebsd.org/base/stable/${FREEBSD_VERSION} /usr/src
 
@@ -16,6 +17,10 @@ make installworld
 cp -R /etc /etc.old && cd /usr/src && make distribution DESTDIR=/
 cd /etc.old && cp group passwd master.passwd /etc && pwd_mkdb /etc/master.passwd
 cp /etc.old/ssh/sshd_config /etc/ssh/
+make -DBATCH_DELETE_OLD_FILES delete-old
 
+END=$(date +%s)
+DIFF=$(echo "$END - $START" | bc)
+ELAPSED=$(printf '%02dh:%02dm:%02ds\n' $(($DIFF/3600)) $(($DIFF%3600/60)) $(($DIFF%60)))
 
-echo "rebot and yes | make delete-old-libs"
+echo "Done in: $ELAPSED, rebot and then \"yes | make delete-old-libs\""
